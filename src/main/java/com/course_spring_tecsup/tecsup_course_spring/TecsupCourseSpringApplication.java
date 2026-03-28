676d9ca131162646f0d36785b6ac62f39aedc97f
hermitdev2399-ui <hermitdev2399@gmail.com>
2026-03-28 09:27:38 -0500

    Initial commit

---
+++ src/main/java/com/course_spring_tecsup/tecsup_course_spring/TecsupCourseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.course_spring_tecsup.tecsup_course_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecsupCourseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecsupCourseSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course_spring_tecsup/tecsup_course_spring/TecsupCourseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course_spring_tecsup.tecsup_course_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecsupCourseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
